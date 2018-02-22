b37e15734a4da880900347bc5efb76cd2277694d
Reshawn Ramjattan <reshawnramjattan@gmail.com>
2018-02-21 21:37:31 -0400

    Added geolocation promise, zoom and pan limits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { VoWPage } from '../pages/vo-w/vo-w';
@@ -53,6 +54,7 @@ import {
     StatusBar,
     SplashScreen,
     GoogleMaps,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,14 +1,7 @@
 import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-// import {
-//   GoogleMaps,
-//   GoogleMap,
-//   GoogleMapsEvent,
-//   GoogleMapOptions,
-//   CameraPosition,
-//   MarkerOptions,
-//   Marker
-//  } from '@ionic-native/google-maps';
+import { Geolocation } from '@ionic-native/geolocation';
+
  declare var google;
 
 @IonicPage()
@@ -19,61 +12,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class MapPage {
   map: any;
   @ViewChild('map') mapElement: ElementRef;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
   }
 
   ionViewDidLoad() {
     this.loadMap();
   }
   loadMap(){
- 
-    let latLng = new google.maps.LatLng(10.640997, -61.400261);
- 
-    let mapOptions = {
-      center: latLng,
-      zoom: 18,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.geolocation.getCurrentPosition().then((position) => {
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        minZoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      // Bounds for Trinidad
+      var strictBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(10.150, -61.564), 
+        new google.maps.LatLng(10.737, -61.11)
+      );
+
+      var center;
+      // Listen for the dragend event
+      var map = this.map;
+      this.map.addListener('dragend', function() {
+        var center = map.getCenter();
+        if (strictBounds.contains(center)) return;
+        console.log("outta here");
+        // out of bounds - Move the map back within the bounds
+
+        var c = center,
+            x = c.lng(),
+            y = c.lat(),
+            maxX = strictBounds.getNorthEast().lng(),
+            maxY = strictBounds.getNorthEast().lat(),
+            minX = strictBounds.getSouthWest().lng(),
+            minY = strictBounds.getSouthWest().lat();
+
+        if (x < minX) x = minX;
+        if (x > maxX) x = maxX;
+        if (y < minY) y = minY;
+        if (y > maxY) y = maxY;
+
+        map.setCenter(new google.maps.LatLng(y, x));
+      });
+    }, (err) => {
+      console.log(err);
+    });
+
+    
   }
 
-  // loadMap() {
-
-  //   let mapOptions: GoogleMapOptions = {
-  //     camera: {
-  //       target: {
-  //         lat: 43.0741904,
-  //         lng: -89.3809802
-  //       },
-  //       zoom: 18,
-  //       tilt: 30
-  //     }
-  //   };
-
-  //   this.map = GoogleMaps.create('map_canvas', mapOptions);
-
-  //   // Wait the MAP_READY before using any methods.
-  //   this.map.one(GoogleMapsEvent.MAP_READY)
-  //     .then(() => {
-  //       console.log('Map is ready!');
-
-  //       // Now you can use all methods safely.
-  //       this.map.addMarker({
-  //           title: 'Ionic',
-  //           icon: 'blue',
-  //           animation: 'DROP',
-  //           position: {
-  //             lat: 43.0741904,
-  //             lng: -89.3809802
-  //           }
-  //         })
-  //         .then(marker => {
-  //           marker.on(GoogleMapsEvent.MARKER_CLICK)
-  //             .subscribe(() => {
-  //               alert('clicked');
-  //             });
-  //         });
-
-  //     });
-  //   }
+ 
 }
