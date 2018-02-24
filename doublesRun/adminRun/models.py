83d9bb6ac9cd76253f374354033957800a97e100
NirvanS <nirvan.sharma95@gmail.com>
2018-02-24 14:30:41 -0400

    Fixed Picture uploading
    Fixed the model in ImageField

--- doublesRun/adminRun/models.py
+++ doublesRun/adminRun/models.py
@@ -9,7 +9,7 @@ class vendor(models.Model):
     Description = models.CharField(max_length=200)
     locLat = models.DecimalField(max_digits=19, decimal_places=10)
     locLong = models.DecimalField(max_digits=19, decimal_places=10)
-    pic = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    pic = models.ImageField(upload_to='media/None/', height_field=None, width_field=None, max_length=100)
 
     def __str__(self):
         return self.Name
