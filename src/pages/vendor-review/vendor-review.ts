54362e6e1abf5441d63627dc7ca0f4db86e8eed7
Reshawn Ramjattan <reshawnramjattan@gmail.com>
2018-03-14 01:00:27 -0400

    Add user to db if not already added

--- src/pages/vendor-review/vendor-review.ts
+++ src/pages/vendor-review/vendor-review.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http, Headers } from '@angular/http';
+import { LoginProvider } from '../../providers/login/login'
 /**
  * Generated class for the VendorReviewPage page.
  *
@@ -17,7 +18,10 @@ export class VendorReviewPage {
   name: String;
   description: String;
   type: boolean;
-  
+  currUserID: string;
+  uFName: string;
+  uLName: string;
+  uEmail: string;
 
   
   review = {
@@ -37,13 +41,17 @@ export class VendorReviewPage {
     vendorID: ""
   }
   
-  constructor(public navCtrl: NavController, private http: Http, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, private http: Http, public navParams: NavParams, public loginProvider: LoginProvider) {
     this.name = navParams.get('vendorName');
     this.description = navParams.get('vendorDescription');
     this.type = navParams.get('vendorType');
     this.review.vendorID = navParams.get('vendorID');
     this.NonReview.vendorID = navParams.get('vendorID');
-    console.log("vendorid:",this.review.vendorID);
+    
+    this.currUserID  = this.loginProvider.userId;
+    this.uFName = this.loginProvider.givenName;
+    this.uLName = this.loginProvider.familyName;
+    this.uEmail = this.loginProvider.email;
   }
   
 
@@ -80,6 +88,18 @@ export class VendorReviewPage {
       .subscribe(data => {
         console.log("httppost responsea:",data);
         
+      });
+    this.http.get('http://127.0.0.1:8000/users/'+this.currUserID+'/')
+      .map(res=>res.json())
+      .subscribe(data=>{},err=>{
+        if (err.status == 404){
+          let newuser = {id:this.currUserID, name:this.uFName+' '+this.uLName, email: this.uEmail};
+          this.http.post('http://127.0.0.1:8000/users/'+this.currUserID+'/', JSON.stringify(newuser),{headers: headers})
+          .map(res => res.json())
+          .subscribe(data => {
+            console.log("httppost responsea:",data);
+          });
+        }
       })
   }
 
