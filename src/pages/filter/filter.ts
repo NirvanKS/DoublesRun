f9edc90f1e69b4e17336cfb8f25733704dfd5c69
NirvanS <nirvan.sharma95@gmail.com>
2018-04-03 22:23:00 -0400

    Finished Caching and Pulling from Cache in Ranking, Suggested and Filter-List page
    Also tested the snap to map provider so that choosing a vendor from a list would send you to the vendor's location on the map

--- src/pages/filter/filter.ts
+++ src/pages/filter/filter.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FilterListPage } from '../filter-list/filter-list';
 import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { CacheService } from 'ionic-cache';
 
 /**
  * Generated class for the FilterPage page.
@@ -16,53 +18,90 @@ import { Http, Headers } from '@angular/http';
   templateUrl: 'filter.html',
 })
 export class FilterPage {
-  apiUrl="https://dream-coast-60132.herokuapp.com/";
-  vendors:any;
-  highRated:boolean = false;
-  thick:boolean = false;
-  thin:boolean = false;
-  cucumber:boolean = false;
-  spicy:boolean = false;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams,private http: Http) {
+  apiUrl = "https://dream-coast-60132.herokuapp.com/";
+  vendors: any;
+  highRated: boolean = false;
+  thick: boolean = false;
+  thin: boolean = false;
+  cucumber: boolean = false;
+  spicy: boolean = false;
+  cachedVendors: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, private cache: CacheService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FilterPage');
   }
 
-  openPage(){
-    //console.log(this.thick,this.thin,this.cucumber,this.spicy);
-    this.http.get(this.apiUrl+'vendors/').map(res => res.json()).subscribe((data: Object) => {
+  loadFromCache(vendorObservable: Observable<any>) {
+
+    vendorObservable.subscribe((data: Object) => {
       this.vendors = Object.values(data);
       //console.log(this.vendors);
-      if(this.highRated)
-      {
+      if (this.highRated) {
         this.vendors = this.vendors.filter(element => element.avgRating >= 4.0);
         //console.log(this.vendors);
       }
-      if(this.thick)
-      {
+      if (this.thick) {
         this.vendors = this.vendors.filter(element => element.avgThickness > 5.0);
         //console.log(this.vendors);
       }
-      if(this.thin)
-      {
+      if (this.thin) {
         this.vendors = this.vendors.filter(element => element.avgThickness <= 5.0);
         //console.log(this.vendors);
       }
-      if(this.cucumber)
-      {
+      if (this.cucumber) {
         this.vendors = this.vendors.filter(element => element.avgCucumber == true);
         //console.log(this.vendors);
       }
-      if(this.spicy)
-      {
+      if (this.spicy) {
         this.vendors = this.vendors.filter(element => element.avgSpicy > 5.0);
         //console.log(this.vendors);
       }
       //console.log(this.vendors);
-      this.navCtrl.push(FilterListPage,{
+      this.navCtrl.push(FilterListPage, {
+        vendors: this.vendors
+      });
+
+
+    });
+
+
+  }
+  openPage() {
+    let url = 'https://dream-coast-60132.herokuapp.com/vendors/';
+    this.cachedVendors = this.cache.loadFromObservable(url, this.http.get(this.apiUrl + 'vendors/').map(res => res.json()));
+    if (this.cachedVendors != null) {
+      console.log("Loading from cache");
+      this.loadFromCache(this.cachedVendors);
+      return;
+    }
+    //console.log(this.thick,this.thin,this.cucumber,this.spicy);
+    this.http.get(this.apiUrl + 'vendors/').map(res => res.json()).subscribe((data: Object) => {
+      this.vendors = Object.values(data);
+      //console.log(this.vendors);
+      if (this.highRated) {
+        this.vendors = this.vendors.filter(element => element.avgRating >= 4.0);
+        //console.log(this.vendors);
+      }
+      if (this.thick) {
+        this.vendors = this.vendors.filter(element => element.avgThickness > 5.0);
+        //console.log(this.vendors);
+      }
+      if (this.thin) {
+        this.vendors = this.vendors.filter(element => element.avgThickness <= 5.0);
+        //console.log(this.vendors);
+      }
+      if (this.cucumber) {
+        this.vendors = this.vendors.filter(element => element.avgCucumber == true);
+        //console.log(this.vendors);
+      }
+      if (this.spicy) {
+        this.vendors = this.vendors.filter(element => element.avgSpicy > 5.0);
+        //console.log(this.vendors);
+      }
+      //console.log(this.vendors);
+      this.navCtrl.push(FilterListPage, {
         vendors: this.vendors
       });
 
